37edae5a90edc3fffc79753d058999d933096711
Michelle B <mjblevins7@gmail.com>
2017-12-26 22:00:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -94,7 +94,6 @@ function createRock(x) {
        */
     // We should kick of the animation of the rock around here
     //window.requestAnimationFrame(moveDown)
-  }
   window.requestAnimationFrame(moveRock)
 
   // Add the rock to ROCKS so that we can remove all rocks
